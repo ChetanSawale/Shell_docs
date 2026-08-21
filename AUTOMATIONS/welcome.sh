b69f3b7daccb806d66be5049306080f96d123da0
Chetan Sawale <sawalechetan102@gmail.com>
2026-08-21 17:29:50 +0000

    Day 1 Automation

---
+++ AUTOMATIONS/welcome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/mnt/c/windows/System32/WindowsPowerShell/v1.0//powershell.exe -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Welcome Chetan!','Linux Automation')"
